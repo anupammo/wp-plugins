ef9057adf9514ea1a78d451195dee988263c61cf
anupammo <a09051985@gmail.com>
2022-12-06 01:43:02 +0530

    061220220142am

---
+++ wp-plugins.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name:       WP Plugins
+ * Plugin URI:        https://anupammondal.in/wp-plugins/
+ * Description:       Extendify the features of WordPress for new templates, blocks with Elementor.
+ * Version:           1.1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Anupam Mondal
+ * Author URI:        https://anupammondal.in/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://anupammondal.in/update-wp-plugins/
+ * Text Domain:       wp-plugins
+ * Domain Path:       /languages
+ */
